b1c4bbfa4b138547cdf61dd789c5eed8ccc601c0
Your Name <you@example.com>
2023-07-28 20:17:23 +0300

    added flake8 info and pywebio and some string theory

---
+++ cool_user_integration.py
@@ -0,0 +1,17 @@
+# from pywebio.input import input as pw_input
+# from pywebio.input import NUMBER, PASSWORD, TEXT, DATETIME, COLOR
+# from pywebio.output import put_success, put_warning, put_code, popup, put_markdown, toast
+#
+# user_password = pw_input('Enter your password ')
+# put_success(user_password)
+#
+# age = pw_input('Enter your age', type=NUMBER)
+# put_warning(age)
+#
+# date = pw_input('Enter your birthday', type=DATETIME)
+# put_warning(f'Your birthday is {date} \U0001F60E')
+
+# popup('popup')
+# put_markdown('put markdown')
+# toast('toast')
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import getpass
+user_pass = getpass.getpass('Enter ur password>>>')
+print(user_pass)
+
+print('*' * 50)
+
+# print(some_string)
+# print(id(some_string))
+#
+# new_string = some_string.lower()
+# print(some_string)
+# print(new_string)
+
+# print(some_string.center(20, '*'))
+# print(some_string.lower())
+# print(some_string.upper())
+
+# print(some_string.replace('My', 'Your',5))
+# print(some_string.casefold())
+# print(some_string.capitalize())
+# print(some_string.swapcase())
+# print(some_string.title())
+# some_string = some_string.replace('M', 'N')
+# print(some_string.replace('N', 'P'))
+
+# translate_map = str.maketrans('MN', 'NP')
+# translate_result = some_string.translate(translate_map)
+# print(translate_result)
+
+some_string = 'My name is Nata. My name is Marta'
+user_name = input('Enter your name >>> ').title()
+print(user_name)
+
+print('*' * 50)
